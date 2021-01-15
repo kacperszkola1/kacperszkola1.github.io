77ab6e0e2aca5ac3e21f13851fea3ab95b230f78
kacperszkola1 <77489965+kacperszkola1@users.noreply.github.com>
2021-01-15 12:32:19 +0100

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,20 @@
+function skakanka1() {
+	document.getElementById("skakankaObraz").src="skakanka1.png";
+	document.getElementById("tytul").innerHTML="Skakanka z licznikiem - 59,99 zł";
+	document.getElementById("odnosnik").href="https://www.decathlon.pl/p/skakanka-z-licznikiem/_/R-p-8956?mc=8556909&c=Czerwony";
+}
+function skakanka2() {
+	document.getElementById("skakankaObraz").src="skakanka2.png";
+	document.getElementById("tytul").innerHTML="Skakanka z obciążeniem - 49,99 zł";
+	document.getElementById("odnosnik").href="https://www.decathlon.pl/p/skakanka-z-obci-eniem/_/R-p-14233?mc=8291238&c=Szary";
+}
+function skakanka3() {
+	document.getElementById("skakankaObraz").src="skakanka3.png";
+	document.getElementById("tytul").innerHTML="Skakanka do szybkiego skakania - 44,99 zł";
+	document.getElementById("odnosnik").href="https://www.decathlon.pl/p/skakanka-do-szybkiego-skakania/_/R-p-308611?mc=8548811&c=Czarny";
+}
+function skakanka4() {
+	document.getElementById("skakankaObraz").src="skakanka4.png";
+	document.getElementById("tytul").innerHTML="Skakanka DZIK - 32,00 zł";
+	document.getElementById("odnosnik").href="https://wksklep.pl/skakanka-dzik-r";
+}
